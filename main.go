cecf043bb21b3ebb86185594d2e3ee9f88c08357
Tieg Zaharia <tieg.zaharia@gmail.com>
2018-08-26 18:23:27 -0400

    Forgot to commit main.go.

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+  "fmt"
+  "./sparkey"
+)
+
+func main () {
+  // TODO add cmd-line flags here
+
+  // TEMP example code
+  // s := sparkey.New("sparkey_db", sparkey.COMPRESSION_NONE, 1024)
+  // s.Put("first", "Hello")
+  // s.Put("second", "Worlb")
+  // s.Put("third", "Goodbye")
+  // s.Put("fourth", "EOM")
+  // s.Delete("third")
+  // s.Flush()
+
+  // s.PrettyPrintHash();
+
+  // s.Get("first")
+  // fmt.Printf("Sparkey info:\n\n")
+  // fmt.Printf("  Basename:\t\t%s\n", s.Basename)
+  // fmt.Printf("  CompressionType:\t\t%d\n", s.CompressionType)
+  // fmt.Printf("  Size:\t\t%d\n", s.Size())
+  // fmt.Printf("  LogWriter.CompressionType:\t\t%d\n", s.CompressionType)
+  // fmt.Printf("  LogWriter.BlockSize:\t\t%d\n", s.BlockSize)
+  // fmt.Printf("  MaxKeyLen:\t\t%d\n", s.MaxKeyLen())
+  // fmt.Printf("  MaxValueLen:\t\t%d\n", s.MaxValueLen())
+  // s.Close()
+
+}
